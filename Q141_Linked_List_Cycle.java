c95a56ad81578229733d6095294864ded76b4b64
divyaprakash <d.prakash1506@gmail.com>
2022-10-10 23:47:16 +0530

    added sol for Q141_Linked_List_Cycle

---
+++ Q141_Linked_List_Cycle.java
@@ -0,0 +1,25 @@
+public class Q141_Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
